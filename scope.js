c7bff0dedc9167812cf5ee9532381f7327ccf5fb
Hyeong-Yun <guddbs5545@inu.ac.kr>
2022-03-09 12:08:22 +0900

    1th: scope practice

---
+++ scope.js
@@ -0,0 +1,30 @@
+// example global, local
+var a = 1; //global scope
+
+function show(){
+  var a = 123; //local scope
+  console.log(a); // 123
+}
+
+show();
+
+console.log(a) // 1
+
+
+// scope chain, function scope
+var a = 1; 
+
+function show(){
+  console.log(a); // 1
+}
+
+show();
+
+// block scope
+function show(){
+  console.log(a) //no result
+}
+
+{//block
+  const a = '1'; // only available in this block
+}
