1a2339146c11d78cb5bf19233d921333471798c2
HyeongYun <guddbs5545@naver.com>
2022-06-21 23:13:57 +0900

    develop 브랜치 생성

--- scope.js
+++ scope.js
@@ -1,30 +1,32 @@
 // example global, local
 var a = 1; //global scope
 
-function show(){
+function show() {
   var a = 123; //local scope
   console.log(a); // 123
 }
 
 show();
 
-console.log(a) // 1
-
+console.log(a); // 1
 
 // scope chain, function scope
-var a = 1; 
+var a = 1;
 
-function show(){
+function show() {
   console.log(a); // 1
 }
 
 show();
 
 // block scope
-function show(){
-  console.log(a) //no result
+function show() {
+  console.log(a); //no result
 }
 
-{//block
-  const a = '1'; // only available in this block
+{
+  //block
+  const a = "1"; // only available in this block
 }
+
+// develope 브랜치 생성
